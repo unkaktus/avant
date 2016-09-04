a0fecc79632d13b7efdbb3497db2f89e4c16db3a
Ivan Markin <twim@riseup.net>
2016-09-04 15:30:02 +0000

    go fmt + some refactoring

--- avant.go
+++ avant.go
@@ -8,44 +8,41 @@
 package main
 
 import (
-    "flag"
-    "log"
-    "os"
-    "io/ioutil"
-    "fmt"
-    "github.com/nogoegst/onionutil"
-    badrand "math/rand"
-    "bulb"
-    bulb_utils "bulb/utils"
+	"bulb"
+	bulb_utils "bulb/utils"
+	"flag"
+	"fmt"
+	"github.com/nogoegst/onionutil"
+	"io/ioutil"
+	"log"
+	badrand "math/rand"
+	"os"
 )
 
 const MAX_REPLICA_NUMBER = 6
 const MAX_INTROPOINT_NUMBER = 10
 
-func shuffleIntroPoints(src []onionutil.IntroductionPoint) (
-                        dst []onionutil.IntroductionPoint) {
-    dst = make([]onionutil.IntroductionPoint, len(src))
-    perm := badrand.Perm(len(src))
-    for index,value := range perm {
-        dst[value] = src[index]
-    }
-    return dst
+func shuffleIntroPoints(src, dst []onionutil.IntroductionPoint) {
+	perm := badrand.Perm(len(src))
+	for index, value := range perm {
+		dst[value] = src[index]
+	}
 }
 
 /* 6-bit mask for 'enable-publish' bit for each replica */
-func parseReplicaMask(mask string)(bool_mask [MAX_REPLICA_NUMBER]bool, err error) {
-    if len(mask) != MAX_REPLICA_NUMBER {
-        return bool_mask, fmt.Errorf("Wrong mask length - should be %d",
-                                     MAX_REPLICA_NUMBER)
-    }
-    for i, v := range mask {
-        if v != '0' {
-            bool_mask[i] = true
-        } else {
-            bool_mask[i] = false
-        }
-    }
-    return bool_mask, nil
+func parseReplicaMask(mask string) (bool_mask [MAX_REPLICA_NUMBER]bool, err error) {
+	if len(mask) != MAX_REPLICA_NUMBER {
+		return bool_mask, fmt.Errorf("Wrong mask length - should be %d",
+			MAX_REPLICA_NUMBER)
+	}
+	for i, v := range mask {
+		if v != '0' {
+			bool_mask[i] = true
+		} else {
+			bool_mask[i] = false
+		}
+	}
+	return bool_mask, nil
 }
 
 /* pickIntroPoints picks introduction points from all_ips and populates
@@ -53,81 +50,81 @@ func parseReplicaMask(mask string)(bool_mask [MAX_REPLICA_NUMBER]bool, err error
  * option is true it does use layover anyway.
  */
 func pickIntroPoints(all_ips []onionutil.IntroductionPoint, distinct_descs bool) (
-                     ipForReplica [][]onionutil.IntroductionPoint) {
-    ipForReplica = make([][]onionutil.IntroductionPoint,
-                             MAX_REPLICA_NUMBER)
-    all_ips = shuffleIntroPoints(all_ips)
-    if (len(all_ips) <= MAX_INTROPOINT_NUMBER && distinct_descs == false) {
-        for i, _ := range ipForReplica {
-            ipForReplica[i] = all_ips
-        }
-    } else { /* Distinct descriptors mode */
-        /* Truncate if it's too many IPs */
-        if len(all_ips) > MAX_REPLICA_NUMBER*MAX_INTROPOINT_NUMBER {
-            all_ips = all_ips[:MAX_REPLICA_NUMBER*MAX_INTROPOINT_NUMBER]
-        }
-        /* Distribute IPs using layover method */
-        for i, ip := range all_ips {
-            index := i%MAX_REPLICA_NUMBER
-            ipForReplica[index] = append(ipForReplica[index], ip)
-        }
-    }
-    return ipForReplica
+	ipForReplica [][]onionutil.IntroductionPoint) {
+	ipForReplica = make([][]onionutil.IntroductionPoint,
+		MAX_REPLICA_NUMBER)
+	shuffleIntroPoints(all_ips, all_ips)
+	if len(all_ips) <= MAX_INTROPOINT_NUMBER && distinct_descs == false {
+		for i, _ := range ipForReplica {
+			ipForReplica[i] = all_ips
+		}
+	} else { /* Distinct descriptors mode */
+		/* Truncate if it's too many IPs */
+		if len(all_ips) > MAX_REPLICA_NUMBER*MAX_INTROPOINT_NUMBER {
+			all_ips = all_ips[:MAX_REPLICA_NUMBER*MAX_INTROPOINT_NUMBER]
+		}
+		/* Distribute IPs using layover method */
+		for i, ip := range all_ips {
+			index := i % MAX_REPLICA_NUMBER
+			ipForReplica[index] = append(ipForReplica[index], ip)
+		}
+	}
+	return ipForReplica
 }
 
 func main() {
-    var debug_flag = flag.Bool("debug", false,
-        "Show what's happening")
-    var save_to_files = flag.Bool("save-to-files", false,
-        "Save descriptors to files 'onion.replica.desc' in the working directory")
-    var distinct_descs = flag.Bool("distinct-descs", false,
-        "Force distinct descriptors mode")
-    var control = flag.String("control-addr", "tcp://127.0.0.1:9051",
-        "Set Tor control address to be used")
-    var control_passwd = flag.String("control-passwd", "",
-        "Set Tor control auth password")
-    var replica_mask = flag.String("replica-mask","111111",
-        "Select replicas to publish descriptors")
-    flag.Usage = func() {
-        fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-        fmt.Fprintf(os.Stderr, "%s [-flags] frontonion backonion1 [backonion2 [...]]\n",
-                    os.Args[0])
-        fmt.Fprintf(os.Stderr, "Available flags:\n")
-        flag.PrintDefaults()
-    }
-    flag.Parse()
-    debug := *debug_flag
-    var tail = flag.Args()
-    switch {
-        case len(tail) < 1:
-            log.Fatalf("You must specify at least one frontend onion")
-        case len(tail) < 2:
-            log.Fatalf("You must specify at least one backend onion")
-    }
-    replicas, err := parseReplicaMask(*replica_mask)
-    if err != nil {
-        log.Fatalf("Wrong replica mask: %v", err)
-    }
-    var front_onion = tail[0]
-    var onions = tail[1:]
+	var debug_flag = flag.Bool("debug", false,
+		"Show what's happening")
+	var save_to_files = flag.Bool("save-to-files", false,
+		"Save descriptors to files 'onion.replica.desc' in the working directory")
+	var distinct_descs = flag.Bool("distinct-descs", false,
+		"Force distinct descriptors mode")
+	var control = flag.String("control-addr", "tcp://127.0.0.1:9051",
+		"Set Tor control address to be used")
+	var control_passwd = flag.String("control-passwd", "",
+		"Set Tor control auth password")
+	var replica_mask = flag.String("replica-mask", "111111",
+		"Select replicas to publish descriptors")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "%s [-flags] frontonion backonion1 [backonion2 [...]]\n",
+			os.Args[0])
+		fmt.Fprintf(os.Stderr, "Available flags:\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	debug := *debug_flag
+	var tail = flag.Args()
+	switch {
+	case len(tail) < 1:
+		log.Fatalf("You must specify at least one frontend onion")
+	case len(tail) < 2:
+		log.Fatalf("You must specify at least one backend onion")
+	}
+	replicas, err := parseReplicaMask(*replica_mask)
+	if err != nil {
+		log.Fatalf("Wrong replica mask: %v", err)
+	}
+	var front_onion = tail[0]
+	var onions = tail[1:]
 
-    log.Printf("Trying to get public key for the front onion...")
-    perm_pk, err := getPubKeyFor(front_onion)
-    if err != nil {
-        log.Fatalf("Cannot get front onion pubkey: %v", err)
-    }
-    // Check if we've got the right key
-    permid_from_pk, _ := onionutil.CalcPermanentID(perm_pk)
-    if onionutil.Base32Encode(permid_from_pk) != front_onion {
-        log.Fatalf("We've got wrong public key for the front onion")
-    }
-    // Parse control string
-    control_net, control_addr, err := bulb_utils.ParseControlPortString(*control)
-    if err != nil {
-        log.Fatalf("Failed to parse Tor control address string: %v", err)
-    }
-    // Connect to a running tor instance.
-    c, err := bulb.Dial(control_net, control_addr)
+	log.Printf("Trying to get public key for the front onion...")
+	perm_pk, err := getPubKeyFor(front_onion)
+	if err != nil {
+		log.Fatalf("Cannot get front onion pubkey: %v", err)
+	}
+	// Check if we've got the right key
+	permid_from_pk, _ := onionutil.CalcPermanentID(perm_pk)
+	if onionutil.Base32Encode(permid_from_pk) != front_onion {
+		log.Fatalf("We've got wrong public key for the front onion")
+	}
+	// Parse control string
+	control_net, control_addr, err := bulb_utils.ParseControlPortString(*control)
+	if err != nil {
+		log.Fatalf("Failed to parse Tor control address string: %v", err)
+	}
+	// Connect to a running tor instance.
+	c, err := bulb.Dial(control_net, control_addr)
 	if err != nil {
 		log.Fatalf("Failed to connect to control socket: %v", err)
 	}
@@ -150,98 +147,98 @@ func main() {
 	}
 	log.Printf("We're using tor %v", resp.Data[0])
 
-    c.StartAsyncReader()
+	c.StartAsyncReader()
 
-    // Initialize fetches for all the onions
-    log.Printf("Sending fetch requests for all descriptors...")
-    for _, onion := range onions {
-        resp, err := c.Request("HSFETCH %v", onion)
-        if err != nil {
-            log.Fatalf("HSFETCH failed: %v", err)
-        }
-        if debug {
-            log.Printf("HSFETCH response: %v", resp)
-        }
-    }
+	// Initialize fetches for all the onions
+	log.Printf("Sending fetch requests for all descriptors...")
+	for _, onion := range onions {
+		resp, err := c.Request("HSFETCH %v", onion)
+		if err != nil {
+			log.Fatalf("HSFETCH failed: %v", err)
+		}
+		if debug {
+			log.Printf("HSFETCH response: %v", resp)
+		}
+	}
 
-    if _, err := c.Request("SETEVENTS HS_DESC_CONTENT"); err != nil {
+	if _, err := c.Request("SETEVENTS HS_DESC_CONTENT"); err != nil {
 		log.Fatalf("SETEVENTS HS_DESC_CONTENT failed: %v", err)
 	}
 
-    var descriptors []onionutil.OnionDescriptor
-    // XXX: if some onion is broken we're stuck. timeout?
+	var descriptors []onionutil.OnionDescriptor
+	// XXX: if some onion is broken we're stuck. timeout?
 
-    // Revieve descriptors and parse them
-    for len(onions) > 0 {
-	ev, err := c.NextEvent()
-	if err != nil {
-		log.Fatalf("NextEvent() failed: %v", err)
+	// Revieve descriptors and parse them
+	for len(onions) > 0 {
+		ev, err := c.NextEvent()
+		if err != nil {
+			log.Fatalf("NextEvent() failed: %v", err)
+		}
+		descContent := []byte(ev.Data[1])
+		descs, _ := onionutil.ParseOnionDescriptors(descContent)
+		if len(descs) == 0 {
+			log.Printf("There are no descriptors in this document. Skipping.")
+			if debug {
+				log.Printf("Broken response %v", ev)
+			}
+			continue
+		}
+		for _, desc := range descs {
+			permID, err := onionutil.CalcPermanentID(desc.PermanentKey)
+			if err != nil {
+				log.Printf("Error in calculating permanent id: %v", err)
+				continue
+			}
+			onion_curr := onionutil.Base32Encode(permID)
+			/* Is this onion is among the requested by us? */
+			for i, onion := range onions {
+				if onion_curr == onion {
+					onions = append(onions[:i], onions[i+1:]...)
+					log.Printf("Got descriptor for %v.onion "+
+						"with %d introduction points. "+
+						"%v descriptors left",
+						onion_curr,
+						len(desc.IntroductionPoints),
+						len(onions))
+					descriptors = append(descriptors, desc)
+					break
+				}
+			}
+		}
 	}
-        descContent := []byte(ev.Data[1])
-        descs, _  := onionutil.ParseOnionDescriptors(descContent)
-        if len(descs) == 0  {
-            log.Printf("There are no descriptors in this document. Skipping.")
-            if (debug) {
-                log.Printf("Broken response %v", ev)
-            }
-            continue
-        }
-        for _, desc := range descs {
-            permID, err := onionutil.CalcPermanentID(desc.PermanentKey)
-            if err != nil {
-                log.Printf("Error in calculating permanent id: %v", err)
-                continue
-            }
-            onion_curr := onionutil.Base32Encode(permID)
-            /* Is this onion is among the requested by us? */
-            for i, onion := range onions {
-                if onion_curr == onion {
-                    onions = append(onions[:i], onions[i+1:]...)
-                    log.Printf("Got descriptor for %v.onion "+
-                               "with %d introduction points. "+
-                               "%v descriptors left",
-                               onion_curr,
-                               len(desc.IntroductionPoints),
-                               len(onions))
-                    descriptors = append(descriptors, desc)
-                    break
-                }
-            }
-        }
-    }
 
-    // Gather all the IPs
-    all_ips := make([]onionutil.IntroductionPoint,
-                    0, len(onions)*MAX_INTROPOINT_NUMBER)
-    for _, descriptor := range descriptors {
-        all_ips = append(all_ips, descriptor.IntroductionPoints...)
-    }
+	// Gather all the IPs
+	all_ips := make([]onionutil.IntroductionPoint,
+		0, len(onions)*MAX_INTROPOINT_NUMBER)
+	for _, descriptor := range descriptors {
+		all_ips = append(all_ips, descriptor.IntroductionPoints...)
+	}
 
-    // Pick IPs from the pool
-    picked_ips := pickIntroPoints(all_ips, *distinct_descs)
-    lens := make([]int, len(picked_ips))
-    for i, _ := range picked_ips {
-        lens[i] = len(picked_ips[i])
-    }
-    log.Printf("Using the following IP distribution: %v", lens)
+	// Pick IPs from the pool
+	picked_ips := pickIntroPoints(all_ips, *distinct_descs)
+	lens := make([]int, len(picked_ips))
+	for i, _ := range picked_ips {
+		lens[i] = len(picked_ips[i])
+	}
+	log.Printf("Using the following IP distribution: %v", lens)
 
-    for replica, do_publish := range replicas {
-        desc := onionutil.NewOnionDescriptor(perm_pk, picked_ips[replica], replica)
-        signedDesc := desc.Sign(signWith(front_onion))
+	for replica, do_publish := range replicas {
+		desc := onionutil.NewOnionDescriptor(perm_pk, picked_ips[replica], replica)
+		signedDesc := desc.Sign(signWith(front_onion))
 
-        if (*save_to_files) {
-            ioutil.WriteFile(fmt.Sprintf("%v.%v.desc", front_onion, replica),
-                             signedDesc, 0600)
-        }
-        if do_publish {
-            log.Printf("Publishing descriptor under replica #%v", replica)
-            resp, _ = c.Request("+HSPOST\n%s.", signedDesc)
-            if debug {
-                log.Printf("HSPOST response: %v", resp)
-            }
-        }
-    }
+		if *save_to_files {
+			ioutil.WriteFile(fmt.Sprintf("%v.%v.desc", front_onion, replica),
+				signedDesc, 0600)
+		}
+		if do_publish {
+			log.Printf("Publishing descriptor under replica #%v", replica)
+			resp, _ = c.Request("+HSPOST\n%s.", signedDesc)
+			if debug {
+				log.Printf("HSPOST response: %v", resp)
+			}
+		}
+	}
 
-    defer c.Close()
+	defer c.Close()
 
 }
